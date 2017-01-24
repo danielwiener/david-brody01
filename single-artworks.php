7bdecf16170cb878481de3d3740127a5a440e69f
Daniel Wiener <daniel@danielwiener.com>
2017-01-24 11:39:14 -0500

    switching the direction of the previous and next

--- single-artworks.php
+++ single-artworks.php
@@ -25,7 +25,7 @@ get_header(); ?>
 
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<div class="row"><div class="col-lg-9 col-sm-8 col-xs-8"><h1 class="page-title"><?php the_title( ); ?></h1></div><div class="col-lg-3 col-sm-4 col-xs-4"><div class="dw_image_nav">
-					<?php previous_post_link( '%link', '<i class="fa fa-chevron-left icon-2x"></i>', TRUE, ' ', 'portfolios' ); ?> &nbsp;<a href="<?php echo site_url(); ?>/portfolio/<?php echo $portfolio_slug ?>"><i class="fa fa-th icon-2x"></i></a> &nbsp;<?php next_post_link( '%link', '<i class="fa fa-chevron-right icon-2x"></i>', TRUE, ' ', 'portfolios' ); ?></div></div>
+					<?php next_post_link( '%link', '<i class="fa fa-chevron-left icon-2x"></i>', TRUE, ' ', 'portfolios' ); ?> &nbsp;<a href="<?php echo site_url(); ?>/portfolio/<?php echo $portfolio_slug ?>"><i class="fa fa-th icon-2x"></i></a> &nbsp;<?php previous_post_link( '%link', '<i class="fa fa-chevron-right icon-2x"></i>', TRUE, ' ', 'portfolios' ); ?></div></div>
 
 					<div class="entry-content">
 						
