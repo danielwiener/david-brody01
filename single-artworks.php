e159dab34f7c1e04054fca1452c23871308f2c5c
Daniel Wiener <daniel@danielwiener.com>
2017-01-25 11:38:25 -0500

    added the id "page" for Gestures plugin

--- single-artworks.php
+++ single-artworks.php
@@ -9,7 +9,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="container content-area">
+	<div id="primary page" class="container content-area">
 		<main id="main" class="site-main" role="main">
 
 		<?php
